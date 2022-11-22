03263bb785776e1e7280ee4b201655aae4b6d7d2
Jiří Vrba <jiri@vrba.dev>
2022-11-23 00:10:12 +0100

    Implement formatting of other parameters

--- README.md
+++ README.md
@@ -2,14 +2,19 @@
 
 # Databáze balíčků karet ze hry Magic: The Gathering
 
-Dokument popisuje kolekci balíčků karet ze hry Magic: The Gathering vydanou společností Wizards of the Coast LLC.
+Dokument popisuje kolekci balíčků karet ze hry Magic: The Gathering vydanou
+společností Wizards of the Coast LLC.
 
-Homepage karetní hry s informacemi lze naléze na [https://magic.wizards.com/](https://magic.wizards.com/).
+Homepage karetní hry s informacemi lze naléze na
+[https://magic.wizards.com/](https://magic.wizards.com/).
 
-Každý balíček obsahuje agregované informace o obsažených kartách, název, obrázek a pak samotný seznam karet v balíčku.
+Každý balíček obsahuje agregované informace o obsažených kartách, název, obrázek
+a pak samotný seznam karet v balíčku.
 
-Každá karta má definované vlastnosti podle toho, o jakou kartu se jedná. 
+Každá karta má definované vlastnosti podle toho, o jakou kartu se jedná.
 
-U všech karet se uvádí typ, název, text, barva, rarita, obrázek, autor obrázku, legalita karty (jestli je možné kartu hrát v různých formátech MtG) a edice ve které byla karta vydána.
+U všech karet se uvádí typ, název, text, barva, rarita, obrázek, autor obrázku,
+legalita karty (jestli je možné kartu hrát v různých formátech MtG) a edice ve
+které byla karta vydána.
 
 U některých dalších typů jsou pak vedeny další atributy viz ukázkový dokument.
--- download-arena-deck.ts
+++ download-arena-deck.ts
@@ -2,90 +2,137 @@
 
 import fs from "https://deno.land/std@0.165.0/node/fs.ts";
 import process from "https://deno.land/std@0.165.0/node/process.ts";
-import { copyFilePromise } from "https://deno.land/std@0.165.0/node/_fs/_fs_copy.ts";
+import { _format } from "https://deno.land/std@0.165.0/path/_util.ts";
+
+const capitalize = (input: string): string => input.charAt(0).toUpperCase() + input.slice(1);
 
 const source = fs.readFileSync("./deck.txt").toString();
 const parsed = source.split("\n")
-    .map(line => line.trim())
-    .filter(line => line.length > 0)
-    .map(line => line.split(/\s+/))
-    .map(([count, ...name]) => {
-        return {
-            count: Number(count),
-            name: name.join(" ")
-        }
-    });
+  .map((line) => line.trim())
+  .filter((line) => line.length > 0)
+  .map((line) => line.split(/\s+/))
+  .map(([count, ...name]) => {
+    return {
+      count: Number(count),
+      name: name.join(" "),
+    };
+  });
 
 const data = await Promise.all(
-    parsed.map(({ count, name }) => {
-        return fetch("https://api.scryfall.com/cards/named?exact=" + name.replace(/\s+/g, "+").toLowerCase())
-            .then(response => response.json())
-            .then(response => {
-                return {
-                    count,
-                    data: {
-                        name: response.name,
-                        cost: response.mana_cost,
-                        rarity: response.rarity,
-                        set: response.set,
-                        type: response.type_line,
-                        colors: response.colors,
-                        text: response.oracle_text,
-                        image: response.image_uris.art_crop,
-                        artist: response.artist,
-                        power: response.power,
-                        toughness: response.toughness,
-                        legalities: response.legalities
-                    }
-                }
-            });
-    })
+  parsed.map(({ count, name }) => {
+    return fetch(
+      "https://api.scryfall.com/cards/named?exact=" +
+        name.replace(/\s+/g, "+").toLowerCase(),
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          count,
+          data: {
+            name: response.name,
+            cost: response.mana_cost,
+            rarity: response.rarity,
+            set: response.set,
+            type: response.type_line,
+            colors: response.colors,
+            text: response.oracle_text,
+            image: response.image_uris.art_crop,
+            artist: response.artist,
+            power: response.power,
+            toughness: response.toughness,
+            legalities: response.legalities,
+          },
+        };
+      });
+  }),
 );
 
+type Symbol = "G" | "R" | "W" | "U" | "B";
+type Color = "colorless" | "green" | "red" | "white" | "blue" | "black";
+
+const colors: Record<Symbol, Color> = {
+  "G": "green",
+  "R": "red",
+  "W": "white",
+  "U": "blue",
+  "B": "black",
+};
+
 const processCost = (source: string): string => {
-    type Symbol = "G" | "R" | "W" | "U" | "B";
-    type Color = "colorless" | "green" | "red" | "white" | "blue" | "black";
+  const cost: Record<Color, number> = [
+    ...source.matchAll(/\{([0-9]+|G|R|B|U|W)\}/g),
+  ].reduce((accumulator, match) => {
+    if (match[1].match(/\d+/)) {
+      return { ...accumulator, colorless: Number(match[1]) };
+    }
 
-    const cost: Record<Color, number> = [...source.matchAll(/\{([0-9]+|G|R|B|U|W)\}/g)].reduce((accumulator, match) => {
-        if (match[1].match(/\d+/)) {
-            return { ...accumulator, colorless: Number(match[1]) };
-        }
+    const symbol: Symbol = match[1] as Symbol;
+    const color: Color = colors[symbol];
 
-        const symbol: Symbol = match[1] as Symbol;
-        const colors: Record<Symbol, Color> = {
-            "G": "green",
-            "R": "red",
-            "W": "white",
-            "U": "blue",
-            "B": "black"
-        };
+    return { ...accumulator, [color]: accumulator[color] + 1 };
+  }, {
+    colorless: 0,
+    green: 0,
+    red: 0,
+    white: 0,
+    blue: 0,
+    black: 0,
+  });
 
-        const color: Color = colors[symbol];
+  return Object.entries(cost)
+    .filter(([_color, value]) => value > 0)
+    .map(([color, value]) => "<" + color + ">" + value + "</" + color + ">")
+    .join("\n");
+};
 
-        return { ...accumulator, [color]: accumulator[color] + 1 }
-    }, {
-        colorless: 0,
-        green: 0,
-        red: 0,
-        white: 0,
-        blue: 0,
-        black: 0
-    });
+const processType = (source: string): { type: string; subtype: string } => {
+  const [type, subtype] = (source + " — ").split("—").map((item) =>
+    item.trim()
+  );
 
-    return Object.entries(cost)
-        .filter(([_color, value]) => value > 0)
-        .map(([color, value]) => "<" + color + ">" + value + "</" + color + ">")
-        .join("\n")
+  return {
+    type,
+    subtype,
+  };
+};
+
+const processColors = (source: Array<Symbol>): string => {
+  return source
+    .map(symbol => colors[symbol])
+    .map(color => `<color>${capitalize(color)}</color>`)
+    .join("\n");
 }
 
-const transformed = data.map(card => {
-    return `
+const processLegality = (source: Record<string, string>): string => {
+  return Object.entries(source)
+    .filter(([_format, legality]) => legality === "legal")
+    .map(([format]) => `<format>${format}</format>`)
+    .join("\n");
+}
+
+const transformed = data.map((card) => {
+  const cost = processCost(card.data.cost);
+  const colors = processColors(card.data.colors);
+  const legality = processLegality(card.data.legalities);
+  const { type, subtype } = processType(card.data.type);
+
+  return `
         <card count="${card.count}">
             <name>${card.data.name}</name>
-            <cost>${processCost(card.data.cost)}</cost>
+            <colors>${colors}</colors>
+            <rarity>${capitalize(card.data.rarity)}</rarity>
+            <cost>${cost}</cost>
+            <type>${type}</type>
+            ${subtype && ("<subtype>" + subtype + "</subtype>")},
+            ${card.data.power && ("<power>" + card.data.power + "</power>")}
+            ${card.data.toughness && ("<toughness>" + card.data.toughness + "</toughness>")}
+            <text><![CDATA[${card.data.text}]]></text>
+            <set>${card.data.set}</set>
+            <legality>${legality}</legality>
+            <artist>${card.data.artist}</artist>
         </card>
     `;
 })
-    .join("\n");
+  .join("\n");
 
 process.stdout.write(transformed);
